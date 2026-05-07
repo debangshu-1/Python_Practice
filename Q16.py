ef769f1ab13f828a80b763795136579f23e88471
debangshu-1 <debangshumondal906@gmail.com>
2026-05-08 00:55:02 +0530

    Add files via upload

---
+++ Q16.py
@@ -0,0 +1,10 @@
+print("The numbers from 1 to 100 that are divisible by both 3 and 5 are = ",end = "")
+
+for i in range(1,100):
+    if ((i % 3 == 0) and (i % 5 == 0)):
+        print(i,end=" ")
+print("\n")
+
+'''
+    multiple of 15
+'''
