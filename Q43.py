26a2f702e73ca7c95a5aee482cec10ea821c2aec
debangshu-1 <debangshumondal906@gmail.com>
2026-05-20 21:55:00 +0530

    Add files via upload

---
+++ Q43.py
@@ -0,0 +1,8 @@
+# 1. List of numbers
+numbers = [5, 10, 15, 20, 25]
+
+# 2. List comprehension to get numbers greater than 15
+greater_than_15 = [num for num in numbers if num > 15]
+
+# 3. Print the new list
+print("Numbers greater than 15:", greater_than_15)
